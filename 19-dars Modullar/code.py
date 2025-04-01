687627c82e83ba443f0571607b3b5e7e2c3f99ef
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-01 14:39:03 +0500

    Create code.py

---
+++ 19-dars Modullar/code.py
@@ -0,0 +1,5 @@
+# Ushbu kod IT Creative jamoasi tomonidan yozilgan
+# Muallif: Suyunov Husan
+# Python asoslari: Modullar 19-dars
+# YouTube: https://www.youtube.com/@it_creative
+# GitHub: https://github.com/Xusanbek0039
