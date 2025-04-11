922e46e112a592fa14c0f519d0e706c9eb99cdf5
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-11 09:23:54 +0500

    Update

---
+++ 20-dars amaliy topshiriq/kutubxona.py
@@ -0,0 +1,28 @@
+# kutubxona.py
+
+from mahsulotlar import mahsulotlar
+
+def main():
+    narxlar = []  # Tanlangan mahsulotlarning narxlari ro'yxati
+    
+    while True:
+        # Foydalanuvchidan mahsulot nomini so'rash
+        mahsulot_nomi = input("Mahsulot nomini kiriting: ").lower()
+        
+        # Agar mahsulot mavjud bo'lsa narxlar ro'yxatiga qo'shiladi
+        if mahsulot_nomi in mahsulotlar:
+            narx = mahsulotlar[mahsulot_nomi]
+            narxlar.append(narx)
+            print(f"{mahsulot_nomi.capitalize()} narxi: {narx} so'm")
+        else:
+            print("Bunday mahsulot topilmadi!")
+        
+        # Yana mahsulot tanlashni sorash
+        javob = input("Yana mahsulot tanlaysizmi? (ha/yo'q): ").lower()
+        if javob != 'ha':
+            break
+    
+    # Umumiy to'lov miqdorini hisoblash
+    jami_summa = sum(narxlar)
+    print(f"Siz uchun to'lov miqdori: {jami_summa} so'm")
+
