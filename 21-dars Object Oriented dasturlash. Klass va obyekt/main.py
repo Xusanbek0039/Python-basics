b0a2923da9bbe21c50f750c3f6ee50004a86fa1f
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-20 22:36:22 +0500

    Update

---
+++ 21-dars Object Oriented dasturlash. Klass va obyekt/main.py
@@ -0,0 +1,26 @@
+# Ushbu kod IT Creative jamoasi tomonidan yozilgan
+# Muallif: Suyunov Husan
+# Python asoslari: Modullar 19-dars
+# YouTube: https://www.youtube.com/@it_creative
+# GitHub: https://github.com/Xusanbek0039
+
+
+
+class Oquvchi:
+    def __init__(self,ism,familiya,sinf):
+        self.name = ism
+        self.familiya = familiya
+        self.sinf = sinf
+
+student1 = Oquvchi("Husan","Suyunov",11)
+
+
+print(student1.name)       # Natija: Olim
+print(student1.familiya)  # Natija: Olimov
+print(student1.sinf)      # Natija: 11
+
+
+
+
+
+
