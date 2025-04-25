003901fd8e3f46a3ee5968e79256ef40d56a9617
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-25 09:57:40 +0500

    Update

---
+++ 24-dars Class Inkapsulyatsiyasi/code.py
@@ -0,0 +1,47 @@
+# from uuid import uuid4
+# class Avto:
+#     """Avtomobil klassi"""
+#     def __init__(self,make,model,rang,yil,narh,km=0):
+#         """Avtomobilning xususiyatlari"""
+#         self.make = make
+#         self.model = model
+#         self.rang = rang
+#         self.yil = yil
+#         self.narh = narh
+#         self.__km = km
+#         self.__id = uuid4()
+    
+#     def get_km(self):
+#         return self.__km
+    
+#     def get_id(self):
+#         return self.__id
+
+
+
+
+# print(f"ID: {avto1.get_id()}")
+
+""" 
+3-masala: Foydalanuvchi bilan ishlovchi dastur yozing
+● main() funksiyasi yozing
+● input() orqali foydalanuvchidan triangle yoki square kiritishini so‘rasin
+● Shu nom asosida check_shape() funksiyasini chaqiring
+● Natijani print() qilsin
+● Dastur __name__ == "__main__" orqali ishga tushsin
+"""
+
+
+class Shape:
+    def __init__(self,input_shape):
+        self.input_shape = input_shape
+
+    def main(self):
+        self.input_shape = input("triangle yoki square kiritishini: ")
+    def check_shape(self):
+        if self.input_shape == "triangle":
+            print("triangle shakli")
+        elif self.input_shape == "square":
+            print("square shakli")
+        else:
+            print("nomalum shakil")
---
+++ 24-dars Class Inkapsulyatsiyasi/mala.py
@@ -0,0 +1,34 @@
+class Book:
+    def __init__(self):
+        self.__title = ""
+        self.__author = ""
+        self.__pages = 0
+
+    def set_title(self, title):
+        self.__title = title
+
+    def get_title(self):
+        return self.__title
+
+    def set_author(self, author):
+        self.__author = author
+
+    def get_author(self):
+        return self.__author
+
+    def set_pages(self, pages):
+        if pages > 0:
+            self.__pages = pages
+
+    def get_pages(self):
+        return self.__pages
+
+# Sinov
+book1 = Book()
+book1.set_title("Oq kema")
+book1.set_author("Chingiz Aytmatov")
+book1.set_pages(150)
+
+print("Kitob nomi:", book1.get_title())
+print("Muallifi:", book1.get_author())
+print("Sahifalar soni:", book1.get_pages())
