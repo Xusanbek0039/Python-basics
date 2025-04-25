78055924616240862a4972d9a9026bae4403b621
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-25 16:17:46 +0500

    Update

--- 24-dars Class Inkapsulyatsiyasi/code.py
+++ 24-dars Class Inkapsulyatsiyasi/code.py
@@ -1,26 +1,31 @@
-# from uuid import uuid4
-# class Avto:
-#     """Avtomobil klassi"""
-#     def __init__(self,make,model,rang,yil,narh,km=0):
-#         """Avtomobilning xususiyatlari"""
-#         self.make = make
-#         self.model = model
-#         self.rang = rang
-#         self.yil = yil
-#         self.narh = narh
-#         self.__km = km
-#         self.__id = uuid4()
+from uuid import uuid4
+
+class Avto:
+    """Avtomobil klassi"""
+    def __init__(self,make,model,rang,yil,narh,km=0):
+        """Avtomobilning xususiyatlari"""
+        self.make = make
+        self.model = model
+        self.rang = rang
+        self.yil = yil
+        self.narh = narh
+        self.__km = km
+        self.__id = uuid4()
     
-#     def get_km(self):
-#         return self.__km
+    def get_km(self):
+        return self.__km
     
-#     def get_id(self):
-#         return self.__id
+    def get_id(self):
+        return self.__id
+        
+avto1 = Avto()
+avto1 = get_km(5)
+
+print(get_km()) 
 
 
 
 
-# print(f"ID: {avto1.get_id()}")
 
 """ 
 3-masala: Foydalanuvchi bilan ishlovchi dastur yozing
@@ -32,16 +37,16 @@
 """
 
 
-class Shape:
-    def __init__(self,input_shape):
-        self.input_shape = input_shape
+# class Shape:
+#     def __init__(self,input_shape):
+#         self.input_shape = input_shape
 
-    def main(self):
-        self.input_shape = input("triangle yoki square kiritishini: ")
-    def check_shape(self):
-        if self.input_shape == "triangle":
-            print("triangle shakli")
-        elif self.input_shape == "square":
-            print("square shakli")
-        else:
-            print("nomalum shakil")
+#     def main(self):
+#         self.input_shape = input("triangle yoki square kiritishini: ")
+#     def check_shape(self):
+#         if self.input_shape == "triangle":
+#             print("triangle shakli")
+#         elif self.input_shape == "square":
+#             print("square shakli")
+#         else:
+#             print("nomalum shakil")
