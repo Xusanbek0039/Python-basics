8c5cb7fa5f44776c9843533e36d13e26a6f68e9d
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-04-25 16:49:52 +0500

    Update code.py

--- 24-dars Class Inkapsulyatsiyasi/code.py
+++ 24-dars Class Inkapsulyatsiyasi/code.py
@@ -1,27 +1,26 @@
-from uuid import uuid4
+# from uuid import uuid4
 
-class Avto:
-    """Avtomobil klassi"""
-    def __init__(self,make,model,rang,yil,narh,km=0):
-        """Avtomobilning xususiyatlari"""
-        self.make = make
-        self.model = model
-        self.rang = rang
-        self.yil = yil
-        self.narh = narh
-        self.__km = km
-        self.__id = uuid4()
+# class Avto:
+#     """Avtomobil klassi"""
+#     def __init__(self,make,model,rang,yil,narh,km=0):
+#         """Avtomobilning xususiyatlari"""
+#         self.make = make
+#         self.model = model
+#         self.rang = rang
+#         self.yil = yil
+#         self.narh = narh
+#         self.__km = km
+#         self.__id = uuid4()
     
-    def get_km(self):
-        return self.__km
+#     def get_km(self):
+#         return self.__km
     
-    def get_id(self):
-        return self.__id
-        
-avto1 = Avto()
-avto1 = get_km(5)
+#     def get_id(self):
+#         return self.__id
 
-print(get_km()) 
+# avto1 = Avto("GM","Malibu","Qora",2025,27000,10)
+
+# print(avto1) 
 
 
 
