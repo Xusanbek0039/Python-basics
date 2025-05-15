054ff41501dafbde73cf2ea6a131e25775fc516c
Xusanbek Suyunov <109731657+Xusanbek0039@users.noreply.github.com>
2025-05-15 22:54:04 +0500

    New

---
+++ 27-dars Xatolar bilan ishlash. try-except/main.py
@@ -0,0 +1,187 @@
+"""
+Mentor: Suyunov Husan
+YouTube: https://youtube.com/@it_creative
+Web: https://praktikum.itc.uz
+GitHub: https://github.com/xusanbek0039
+"""
+
+# # 1-kod
+# try:
+#     son = int(input("Son kiriting: "))
+#     print("Natija:", 10 / son)
+# except:
+#     print("Xatolik yuz berdi.")
+
+
+
+
+
+
+
+
+
+
+
+
+# # 2-kod
+# try:
+#     son = int(input("Son kiriting: "))
+#     print("Natija:", 10 / son)
+# except ZeroDivisionError:
+#     print("0 ga bo'lish mumkin emas!")
+# except ValueError:
+#     print("Faqat butun son kiriting!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 3-kod
+# try:
+#     f = open("data.txt")
+#     print(f.read())
+# except FileNotFoundError:
+#     print("Fayl topilmadi.")
+# finally:
+#     print("Dastur tugadi.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 4-kod
+# try:
+#     son = int(input("Son kiriting: "))
+# except ValueError:
+#     print("Xatolik: butun son kiriting!")
+# else:
+#     print("Siz kiritgan son:", son)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 5-kod
+# file_name = input("Fayl nomini kiriting: ")
+# try:
+#     with open(file_name, "r") as f:
+#         print(f.read())
+# except FileNotFoundError:
+#     print("Bu nomdagi fayl mavjud emas.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 6-kod
+# try:
+#     a = int(input("a ni kiriting: "))
+#     b = int(input("b ni kiriting: "))
+#     print("Natija:", a / b)
+# except (ValueError, ZeroDivisionError) as e:
+#     print("Xatolik:", e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 7-kod
+# try:
+#     lst = [1, 2, 3]
+#     print(lst[5])
+# except IndexError:
+#     print("Index mavjud emas.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # 8-kod
+# try:
+#     import math
+#     print(math.sqrt(-1))
+# except ValueError:
+#     print("Manfiy sonning ildizi yo'q.")
