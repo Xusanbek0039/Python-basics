54d8c9b93358a0cf6689fbe6eb8fb0258b2ba867
Xusanbek <109731657+Xusanbek0039@users.noreply.github.com>
2025-05-17 16:14:09 +0500

    New

---
+++ 3-dars print(), arifmetik amallar va sintaksis/main.py
@@ -0,0 +1,40 @@
+"""
+
+Mavzu: Python arifmetik amallar bilan ishlash
+Web: https://husanbek-coder.uz
+YouTube: https://youtube.com/@it_creative
+
+"""
+
+# Kod 1
+# son1 = int(input("Snn 1 ni kiriting: "))
+# son2 = int(input("Son 2 ni kiriting: "))
+
+# print(f"Yig'indisi {son1+son2}, Ayirmasi {son1-son2}")
+
+
+
+
+
+
+
+
+
+
+# Kod 2
+# son1 = int(input("Snn 1 ni kiriting: "))
+# son2 = int(input("Son 2 ni kiriting: "))
+
+# print(f"Bo'linmasi {son1/son2}, Butun bo'linmasi {son1//son2}, Qoldiqli bo'lish {son1%son2}")
+
+
+
+
+
+
+
+
+
+
+
+
