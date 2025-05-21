963dee54a1f9daa8686e2266bbc6cdbf908adc8b
Xusanbek <109731657+Xusanbek0039@users.noreply.github.com>
2025-05-21 15:37:43 +0500

    Update main.py

--- 3-dars print(), arifmetik amallar va sintaksis/main.py
+++ 3-dars print(), arifmetik amallar va sintaksis/main.py
@@ -38,3 +38,26 @@ YouTube: https://youtube.com/@it_creative
 
 
 
+"""
+Foydalanuvchidan quyidagi ma'lumotlar olinadi:
+Kun (hafta kuni)
+Yomg‘ir yog‘yaptimi? (ha yoki yo'q)
+Soat nechi bo‘ldi?
+Shartlar:
+Agar yakshanba kuni bo‘lsa va yomg‘ir yog‘mayotgan bo‘lsa va 
+soat 9:00 dan kichik bo‘lsa, "Tezroq avtobusga chiq, sayr qilamiz!" chiqadi.
+Agar yakshanba bo‘lsa, lekin yomg‘ir yog‘ayotgan bo‘lsa, "Bugun uydan 
+chiqma, yomg‘ir yog‘yapti." chiqadi.
+Boshqa kunlarda esa "Bugun avtobus yo'q, ish qilamiz!" deb chiqariladi.
+
+"""
+kun = input("Hafta kuni: ").lower().strip()
+yomgir = input("Yomg‘ir yog‘yaptimi? (ha yoki yo'q) ")
+soat = int(input("Soat nechchi bo'ldi? "))
+
+if kun == "yakshanba" and yomgir == "ha" and soat <= 9:
+    print("Tezroq avtobusga chiq, sayr qilamiz!")
+elif kun == "shanba":
+    print("Bugun uyda dam olamiz!")
+else:
+    print("Bugun avtobus yo'q, ish qilamiz!")
