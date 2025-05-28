1b8094faf832622d2a0744c44c18d0e67fe6dbc5
Xusanbek <109731657+Xusanbek0039@users.noreply.github.com>
2025-05-28 14:52:14 +0500

    Update

--- dbcon.py
+++ dbcon.py
@@ -1,15 +1,2 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-# ⚠️ Parolingizni quyida almashtiring
-uri = f"mongodb+srv://shoirablog:7tvmi7a8ocpeATju@blog.cbbcsq1.mongodb.net/?retryWrites=true&w=majority&appName=blog"
-
-# Klientni yaratish
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Ping yuborish (bog‘lanishni tekshirish)
-try:
-    client.admin.command('ping')
-    print("MongoDB bilan bog‘lanish muvaffaqiyatli!")
-except Exception as e:
-    print("Bog‘lanishda xatolik:", e)
+ismlar  = ("Husan","Zuhriddin","Donyorbek","Bexruz","Husan","Husan")
+print(ismlar)
