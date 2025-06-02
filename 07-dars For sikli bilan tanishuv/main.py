0a78960d2a654fbc1c474597f8175e709cfb015e
Xusanbek <109731657+Xusanbek0039@users.noreply.github.com>
2025-06-02 14:22:16 +0500

    Update main.py

--- 07-dars For sikli bilan tanishuv/main.py
+++ 07-dars For sikli bilan tanishuv/main.py
@@ -1,11 +1,10 @@
 # # Kod_1
-Mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
+# Mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
 
-for mehmon in Mehmonlar:
-    print(mehmon)
+# for mehmon in Mehmonlar:
+#     print(mehmon)
 
-
-print(Mehmonlar)
+# print(Mehmonlar)
 
 
 
@@ -21,7 +20,7 @@ print(Mehmonlar)
 # mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
 # for mehmon in mehmonlar:
 #     print(f"Hurmatli {mehmon}, sizni 20 Dekabr kuni nahorga oshga taklif qilamiz")
-#     print("Hurmat bilan, Palonchiyevlar oilasi")
+#     print("Hurmat bilan, Palonchiyevlar oilasi\n")
 
 
 
@@ -47,7 +46,7 @@ print(Mehmonlar)
 # mehmonlar = ['Ali','Vali','Hasan', 'Husan','Olim']
 # for mehmon in mehmonlar:
 #     print(f"Hurmatli {mehmon}, sizni 20 Dekabr kuni nahorga oshga taklif qilamiz")
-# print("Hurmat bilan, Palonchiyevlar oilasi\n")
+#     print("Hurmat bilan, Palonchiyevlar oilasi\n")
 
 
 
@@ -127,4 +126,5 @@ print(Mehmonlar)
 # print("5 ta eng yaqin do'stingiz kim?")
 # for n in range(5): # n bu yerda 0 dan 4 gacha qiymatlar oladi
 #     dostlar.append(input(f"{n+1}-do'stingizning ismini kiriting: "))
-# print(dostlar)
+# for dost in dostlar:
+#     print(dost)
